02da77411179848a26785fd8a27c6172de96b7f7
zzb-zzb <2673983214@qq.com>
2023-09-25 17:23:28 +0800

    fix：修改APP内跳转网址

--- vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
+++ vector/src/main/java/im/vector/app/features/settings/VectorSettingsUrls.kt
@@ -17,9 +17,9 @@
 package im.vector.app.features.settings
 
 object VectorSettingsUrls {
-    const val HELP = "https://element.io/help"
-    const val COPYRIGHT = "https://element.io/copyright"
-    const val ACCEPTABLE_USE_POLICY = "https://element.io/acceptable-use-policy-terms"
-    const val PRIVACY_POLICY = "https://element.io/privacy"
+    const val HELP = "https://eim.chat/help"
+    const val COPYRIGHT = "https://eim.chat/copyright"
+    const val ACCEPTABLE_USE_POLICY = "https://eim.chat/acceptable-use-policy-terms"
+    const val PRIVACY_POLICY = "https://eim.chat/privacy"
     const val THIRD_PARTY_LICENSES = "file:///android_asset/open_source_licenses.html"
 }
